5d36b8f4cfc846c66df95e1f52340c12e2b3632d
Umair Pervaiz Butt <umair.pervaiz@developersstudios-MacBook-Air-92.local>
2023-12-15 12:16:22 +0500

    Added seeder data for languages and formats

--- seed-data.ts
+++ seed-data.ts
@@ -1,27 +1,35 @@
 import { getContext } from '@keystone-6/core/context'
-import { countires } from './sample-data'
+import { countires, regions, formats, languages } from './sample-data'
 import config from './keystone'
 import * as PrismaModule from './node_modules/.prisma/client'
 
 async function main () {
   const context = getContext(config, PrismaModule)
-//    let countrsy = await context.db.Country.findOne({
-//     where: { name: 'Pakistan' },
-//   })
-//  console.log(countrsy, 'cCountry')
 
   console.log(`🌱 Inserting seed data`)
   for (const country of countires) {
     console.log(`Adding country: ${country.name}`)
-    // const item = await context.db.Country.findMany({
-    //   where: [{ name: country.name }],
-    // })
-
-    // if (!item) {
       await context.db.Country.createOne({
         data: country,
       })
-    // }
+  }
+  for (const region of regions) {
+    console.log(`Adding region: ${region.name}`)
+      await context.db.Region.createOne({
+        data: region,
+      })
+  }
+  for (const format of formats) {
+    console.log(`Adding format: ${format.name}`)
+      await context.db.Format.createOne({
+        data: format,
+      })
+  }
+  for (const language of languages) {
+    console.log(`Adding language: ${language.name}`)
+      await context.db.Language.createOne({
+        data: language,
+      })
   }
 
   console.log(`✅ Seed data inserted`)
